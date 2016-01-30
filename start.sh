686c3137ce4595e25eccc8744f71caa5d1a01824
nicolas <maximus@debian>
2016-01-30 20:31:48 +0100

    fix password && correct the path of html and conf file

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ if [ ! -f /var/www/sites/default/settings.php ]; then
 	sleep 10s
 	# Generate random passwords 
 	DRUPAL_DB="drupal"
-	MYSQL_PASSWORD=`pwgen -c -n -1 12`
-	DRUPAL_PASSWORD=`pwgen -c -n -1 12`
+	MYSQL_PASSWORD="password"
+	DRUPAL_PASSWORD="password"
 	# This is so the passwords show up in logs. 
 	echo mysql root password: $MYSQL_PASSWORD
 	echo drupal password: $DRUPAL_PASSWORD
@@ -14,7 +14,8 @@ if [ ! -f /var/www/sites/default/settings.php ]; then
 	echo $DRUPAL_PASSWORD > /drupal-db-pw.txt
 	mysqladmin -u root password $MYSQL_PASSWORD 
 	mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE drupal; GRANT ALL PRIVILEGES ON drupal.* TO 'drupal'@'localhost' IDENTIFIED BY '$DRUPAL_PASSWORD'; FLUSH PRIVILEGES;"
-	sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/sites-available/default
+	sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+	sed -i 's/\/var\/www\/html/\/var\/www\//' /etc/apache2/sites-available/000-default.conf
 	a2enmod rewrite vhost_alias
 	cd /var/www/
 	drush site-install standard -y --account-name=admin --account-pass=admin --db-url="mysqli://drupal:${DRUPAL_PASSWORD}@localhost:3306/drupal"
